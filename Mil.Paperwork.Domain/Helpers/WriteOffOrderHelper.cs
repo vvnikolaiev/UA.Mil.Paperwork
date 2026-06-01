b070e2b73ae128a1c7a9348764f952a403606804
vvnikolaiev <vv.nikolaiev@gmail.com>
2026-06-01 18:15:31 +0300

    Revert "fix: add service name (nominative) as bold header in services block"
    This reverts commit 594b505a85efaa81dc02e934f917dbdbfa09bef9.

--- Mil.Paperwork.Domain/Helpers/WriteOffOrderHelper.cs
+++ Mil.Paperwork.Domain/Helpers/WriteOffOrderHelper.cs
@@ -50,10 +50,7 @@ namespace Mil.Paperwork.Domain.Helpers
                 var serviceNameGen = service.ServiceNameGenitive?.ToLower();
 
                 paragraphs.Add(new BlockParagraph(
-                    service.ServiceName ?? string.Empty, IsBold: true, IndentLevel: 0, FontSize: DocumentFontSize));
-
-                paragraphs.Add(new BlockParagraph(
-                    string.Format(ServiceHeaderFormat, serviceNameGen), IsBold: false, IndentLevel: 0, FontSize: DocumentFontSize));
+                    string.Format(ServiceHeaderFormat, serviceNameGen), IndentLevel: 0, FontSize: DocumentFontSize));
 
                 foreach (var asset in service.Assets)
                 {
